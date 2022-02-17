ec69544a19ebaf83e29f2091d39d0255f5e69776
Phung Xuan Anh <phungxuananh1991@gmail.com>
2022-02-17 21:22:57 +0700

    add python script create pair

---
+++ python_script/factory_create_pair.py
@@ -0,0 +1,11 @@
+import json
+from common import w3, DAI_ADDRESS, WETH
+
+factory_built_file = open('../factory/build/contracts/UniswapV2Factory.json', "r")
+factory_built_content = json.load(factory_built_file)
+
+factory_address = factory_built_content['networks']["3"]["address"]
+factory_abi = factory_built_content["abi"]
+factory = w3.eth.contract(address=factory_address, abi=factory_abi)
+receipt = factory.functions.createPair(DAI_ADDRESS, WETH).call()
+print(receipt)
